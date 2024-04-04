3676cd1cc62ee0249f88451340efcd94859466a8
shakib jahan <shakib.jahan@bjitgroup.com>
2024-04-04 08:48:58 +0600

    - Commit all the changes

--- force-app/main/default/lwc/marcoTest/marcoTest.js
+++ force-app/main/default/lwc/marcoTest/marcoTest.js
@@ -103,20 +103,9 @@ import { LightningElement, track } from 'lwc';
                     console.log('Recorded Chunks: ' + recordedChunks);
                     stream.getTracks()
                         .forEach(track => track.stop())
-                    // console.log('here1');
                     const blob = new Blob(recordedChunks, {
                         type:'audio/mp3'
                     });
-                    // console.log('here2');
-                    // const filename = window.prompt('Enter file name');
-                    // recordedChunks = []
-                    // insertFile({ file: blob })
-                    // .then((result) => {
-                    //     console.log('RESULT From BackEnd: ' + result);
-                    // })
-                    // .catch((error) => {
-                    //     console.error('ERRORS: ', error);
-                    // });
                     alert('Downloading file');
 
                     console.log('Blob - ', blob);
@@ -126,7 +115,6 @@ import { LightningElement, track } from 'lwc';
                     reader.readAsDataURL(blob);
                     reader.onloadend = function () {
                         base64String = reader.result;
-                        // console.log('Base64 String - ', base64String);
                         let file = base64String.substring(base64String.indexOf(',') + 1);
                         console.log('FILE: ', file);
 
@@ -140,30 +128,7 @@ import { LightningElement, track } from 'lwc';
                             // Handle error response
                             console.error('Error uploading file: ' + error);
                         });
-                
-                        // Simply Print the Base64 Encoded String, 
-                        // without additional data: Attributes. 
-                        // console.log('Base64 String without Tags- ',
-                        // base64String.substr(base64String.indexOf(', ') + 1));
                     } 
-                    // let fileName = "audioRecord.mp3";
-                    // this.uploadFile(fileName, blob);
-                    // const a = document.createElement('a');
-                    // a.href = URL.createObjectURL(blob);
-                    // a.download = fileName;
-                    // a.click();
-                    // Call Apex method to upload the file
-                    // console.log('Base64 before calling apex: ', base64String);
-                    // insertFile({ file:  base64String })
-                    // .then(result => {
-                    //     // Handle success response
-                    //     console.log('File uploaded successfully: ' + result);
-                    // })
-                    // .catch(error => {
-                    //     // Handle error response
-                    //     console.error('Error uploading file: ' + error);
-                    // });
-
                 };
             })
             .catch((err) => {
@@ -196,6 +161,121 @@ import { LightningElement, track } from 'lwc';
         this.logs.push('Pushed the button end');
         console.log('Ending the recording');
         this.mediaRecorder.stop();
+    }
+
+
+    startVideo() {
+        try {
+            this.logs.push('Pushed the button start');
+            this.logs.push('Navigator platform: ' + navigator.platform);
+            this.logs.push(
+            'Navigator mediadevices: ' +
+                JSON.stringify(navigator.mediaDevices, null, 2)
+            );
+            navigator.mediaDevices
+            .enumerateDevices()
+            .then((devices) => {
+            this.logs.push('Devices: ' + JSON.stringify(devices));
+            });
+            setTimeout(() => {
+                for (const mimeType of types) {
+                    this.logs.push(
+                    'Trying to get the stream for ' +
+                        mimeType +
+                        ' support: ' +
+                        MediaRecorder.isTypeSupported(mimeType)
+                    );
+                }
+            }, 1000);
+            navigator.mediaDevices
+            .getUserMedia({ video: { cursor: "motion" }, audio: { 'echoCancellation': true } })
+            .then((stream) => {
+                alert('Starting the recording');
+                this.logs.push('Starting the recording');  // WE NEVER REACH THIS LINE ON ANDROID
+                console.log('Recording started: ', stream);
+                this.stream = stream;
+                // I want to display the stream in a video element
+                const video = this.template.querySelector('video');
+                video.srcObject = stream;
+                video.play();
+                const audio = this.template.querySelector('audio');
+                audio.srcObject = stream;
+                // audio.play();
+                this.logs.push('We should be live');
+
+
+                const recordedChunks = [];
+
+                this.mediaRecorder = new MediaRecorder(stream);
+                this.mediaRecorder.start();
+                this.mediaRecorder.ondataavailable = function (e) {
+                    alert('Data Available');
+                    console.log('Data available');
+                    if (e.data.size > 0) {
+                        console.log('Recorded chunks available');
+                        recordedChunks.push(e.data);
+                    }
+                };
+
+                this.mediaRecorder.onstop = function (e) {
+                    console.log('Stop called');
+                    alert('Stop Called');
+                    console.log('Recorded Chunks: ' + recordedChunks);
+                    stream.getTracks()
+                        .forEach(track => track.stop())
+                    const blob = new Blob(recordedChunks, {
+                        type:'audio/mp3'
+                    });
+                    alert('Downloading file');
+
+                    console.log('Blob - ', blob);
+
+                    let base64String;
+                    let reader = new FileReader();
+                    reader.readAsDataURL(blob);
+                    reader.onloadend = function () {
+                        base64String = reader.result;
+                        let file = base64String.substring(base64String.indexOf(',') + 1);
+                        console.log('FILE: ', file);
+
+                        console.log('Base64 before calling apex: ', base64String);
+                        insertFile({ file:  file })
+                        .then(result => {
+                            // Handle success response
+                            console.log('File uploaded successfully: ' + result);
+                        })
+                        .catch(error => {
+                            // Handle error response
+                            console.error('Error uploading file: ' + error);
+                        });
+                    } 
+                };
+            })
+            .catch((err) => {
+                this.logs.push('Error: ', JSON.stringify(err));
+                this.logs.push(
+                'err: ' + JSON.stringify(err, Object.getOwnPropertyNames(err), 2)
+                );
+                // How to make sure the error is displayed
+                this.logs.push('err: ' + JSON.stringify(err?.message));
+                this.logs.push('err: ' + JSON.stringify(err?.stack));
+                this.logs.push();
+            });
+        } catch (err) {
+
+            this.logs.push('Error global: ', JSON.stringify(err));
+
+            this.logs.push(
+
+            'error: ' + JSON.stringify(err, Object.getOwnPropertyNames(err), 2)
+
+            );
+
+            this.logs.push('err: ' + JSON.stringify(err.message));
+
+            this.logs.push('err: ' + JSON.stringify(err.stack));
+
+        }
     }
 
     get displayedLogs() {
